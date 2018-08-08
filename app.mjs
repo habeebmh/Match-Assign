c2a7ab4cf7c2f6744c198dd736b6d4bb6f80392d
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2018-08-07 21:42:06 -0500

    Added Sign In Screen

--- app.mjs
+++ app.mjs
@@ -6,6 +6,7 @@ import logger from 'morgan';
 import { __dirname } from './path.mjs'
 
 import indexRouter from './routes/index.mjs';
+import loginRouter from './routes/signin.mjs';
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/signin', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
